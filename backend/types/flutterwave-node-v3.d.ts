ba8834039721af6e486a55a2b88e60704debfb5d
theceo1 <loysiustony@hotmail.com.hk>
2024-07-28 11:26:06 +0100

    no errors for now

--- backend/types/flutterwave-node-v3.d.ts
+++ backend/types/flutterwave-node-v3.d.ts
@@ -1,33 +1,32 @@
 declare module 'flutterwave-node-v3' {
-    interface Customer {
-      email: string;
-      phonenumber: string;
-      name: string;
-    }
-  
-    interface Customizations {
-      title: string;
-      description: string;
-      logo: string;
-    }
-  
-    interface Payload {
-      tx_ref: string;
-      amount: string;
-      currency: string;
-      redirect_url: string;
-      payment_options: string;
-      customer: Customer;
-      customizations: Customizations;
-    }
-  
-    class Flutterwave {
-      constructor(publicKey: string, secretKey: string);
-      Transaction: {
-        initiate(payload: Payload): Promise<any>;
-      };
-    }
-  
-    export = Flutterwave;
+  interface Customer {
+    email: string;
+    phonenumber: string;
+    name: string;
   }
-  
+
+  interface Customizations {
+    title: string;
+    description: string;
+    logo: string;
+  }
+
+  interface Payload {
+    tx_ref: string;
+    amount: string;
+    currency: string;
+    redirect_url: string;
+    payment_options: string;
+    customer: Customer;
+    customizations: Customizations;
+  }
+
+  class Flutterwave {
+    constructor(publicKey: string, secretKey: string);
+    Transaction: {
+      initiate(payload: Payload): Promise<any>;
+    };
+  }
+
+  export = Flutterwave;
+}
